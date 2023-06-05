03b2c548014d45932e6f5e743c141595b16d390c
sachreet <sachreet.kaur@lng-consultancy.com>
2023-06-05 17:31:43 +0530

    Add project files.

---
+++ BikeStoreWebApi/Service/IRabbitMqSender.cs
@@ -0,0 +1,7 @@
+﻿namespace BikeStores.MSSQL.Service
+{
+    public interface IRabbitMqSender
+    {
+        public void PublishToMessageQueue(string integrationEvent, string eventData);
+    }
+}
---
+++ BikeStoreWebApi/Service/RabbitMqSender.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace BikeStores.MSSQL.Service
+{
+    public class RabbitMqSender : IRabbitMqSender
+    {
+        public void PublishToMessageQueue(string integrationEvent, string eventData)
+        {
+            var factory = new ConnectionFactory();
+            using (var connection = factory.CreateConnection())
+            {
+
+                using (var channel = connection.CreateModel())
+                {
+
+                    var body = Encoding.UTF8.GetBytes(eventData);
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(
+                        exchange: "library",
+                        routingKey: integrationEvent,
+                        basicProperties: properties,
+                        body: body);
+
+                }
+            }
+            Console.WriteLine(integrationEvent);
+        }
+    }
+}
